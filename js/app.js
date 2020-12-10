8affcf4f725aa6aba6819634675aed1f8deb7139
Nicole Jimenez <nicole.jimenez@adevinta.com>
2020-12-10 18:23:45 +0100

    Add pie chart for tipo de alumnos

--- js/app.js
+++ js/app.js
@@ -386,6 +386,22 @@ function renderDefaultDashboard() {
 
       mode: WIDGET_TEXT,
     },
+    {
+      width: "1062",
+      height: "300",
+      title: "Distribución de tipo de alumnos",
+      tooltip:
+        "Gráfico circular que describe la distribución por tipo de estudiante",
+      srcJS: "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+      srcCSS: "",
+      mode: WIDGET_CODE_SNIPPET,
+      snippet:
+        "var canvas = document.createElement('canvas');\
+            canvas.id = 'canvas_%ID%';\
+            canvas.width = '%WIDTH%';\
+            canvas.style.width = '%WIDTH%';canvas.height = '%HEIGHT%'-70;canvas.style.height = '%HEIGHT%'-70;document.getElementById('content_%ID%').appendChild(canvas);new Chart(document.getElementById('canvas_%ID%').getContext('2d'), {type: 'pie',options:{tooltips: {bodyFontColor:'#FFFFFF',bodyFontSize:14,bodyFontStyle:'bold',caretSize:0,xPadding:0,yPadding:0},responsive: false,maintainAspectRatio:false,legend:{position:'left'}},data: {labels: %LABELS%,datasets: [{data: %VALUES%,backgroundColor:['rgb(255, 99, 132)','rgb(54, 162, 235)','rgb(255, 205, 86)','rgb(255, 0, 0)','rgb(0, 255, 0)','rgb(0, 0, 255)']}]}});",
+      field: "component",
+    },
     // {
     //   width: 260,
     //   margin_tooltip: 270,
